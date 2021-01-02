a31e72f77659f7b563a6ffccfd6928a58228b669
Arunachalam B <arun@gogosoon.com>
2021-01-02 11:46:27 +0530

    [Arunachalam] Adds simple checking of responses from user input

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World");
+import Prompt from 'prompt-sync';
+
+const prompt = Prompt();
+
+const inputStr = prompt("Enter the characters: ");
+
+const characters: string[] = Array.from(inputStr);
+let countChars = characters.length;
+let response = {correct: 0, wrong: 0};
+
+let showNext = 0;
+
+while(showNext < 5) {
+  const random = Math.round(Math.random() * countChars);
+  console.log("Random", random);
+  console.log("Type: ", characters[random]);
+  const enteredInput = prompt("");
+  if (enteredInput == characters[random]) {
+    response.correct++;
+  } else {
+    response.wrong++;
+  }
+  showNext++;
+}
+
+console.log(`You have ${response.correct} Correct response and ${response.wrong} response`);
+
+console.log("Thank You");

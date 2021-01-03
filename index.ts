80d44ab909ce57363735c338cdae2c39cc6b9bb9
Arunachalam B <arun@gogosoon.com>
2021-01-03 16:30:01 +0530

    [Arunachalam] Adds menu and functionality for fast type checking words

--- index.ts
+++ index.ts
@@ -24,6 +24,10 @@ while(true) {
         fastTypeCharacters();
         break;
       }
+      case menus[1]: {
+        fastTypeWords();
+        break;
+      }
     }
   } else if (Number(selectedOption) == 0) {
     break;
@@ -37,6 +41,56 @@ function fastTypeCharacters() {
   const inputStr = prompt("Enter the characters: ");
   
   const characters: string[] = Array.from(inputStr);
+  let countChars = characters.length;
+  let response = {correct: 0, wrong: 0};
+  
+  let showNext = 0;
+  let userResponse: ResponseObj[] = [];
+  
+  while(showNext < Number(attempts)) {
+    const random = Math.floor(Math.random() * countChars);
+    let responseObj: ResponseObj = {status: null, time: null};
+    
+    console.log("Type: ", characters[random]);
+    
+    console.time((showNext+1).toString());
+    let startTime = Date.now();
+    
+    const enteredInput = prompt("");
+    
+    console.timeLog((showNext+1).toString());
+    let endTime = Date.now();
+  
+    responseObj.time = (endTime-startTime)/1000 + " seconds";
+    
+    if (enteredInput == characters[random]) {
+      response.correct++;
+      console.log(`${GREEN}%s${WHITE}`, '✔');
+      responseObj.status = '✔';
+    } else {
+      response.wrong++;
+      console.log(`${RED}%s${WHITE}`, '✖');
+      responseObj.status = '✖';
+    }
+  
+    userResponse.push(responseObj);
+  
+    console.log(`${WHITE}%s${GREEN}%s${WHITE}%s${RED}%s${WHITE}`, 'Correct: ', response.correct, ' Wrong: ', response.wrong);
+  
+    showNext++;
+  }
+  
+  console.log(`You have ${response.correct} Correct response and ${response.wrong} Wrong response`);
+  
+  userResponse.map((res, index) => {
+    console.log((index+1), res.status, res.time);
+  });
+}
+
+function fastTypeWords() {
+  const attempts = prompt("Enter number of attempts: ");
+  
+  const characters: string[] = ["a", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"];
   let countChars = characters.length;
   let response = {correct: 0, wrong: 0};
   
